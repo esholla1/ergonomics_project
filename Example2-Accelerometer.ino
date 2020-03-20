b36a23ab47c1f656e2327926dc6decdafcd7c425
esholla1 <44717893+esholla1@users.noreply.github.com>
2020-03-19 22:40:52 -0400

    Update Example2-Accelerometer.ino

--- Example2-Accelerometer.ino
+++ Example2-Accelerometer.ino
@@ -1,21 +1,3 @@
-/*
-  Using the BNO080 IMU
-  By: Nathan Seidle
-  SparkFun Electronics
-  Date: December 21st, 2017
-  License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
-
-  Feel like supporting our work? Buy a board from SparkFun!
-  https://www.sparkfun.com/products/14586
-
-  This example shows how to output accelerometer values
-
-  Hardware Connections:
-  Attach the Qwiic Shield to your Arduino/Photon/ESP32 or other
-  Plug the sensor onto the shield
-  Serial.print it out at 9600 baud to serial monitor.
-*/
-
 #include <Wire.h> //Including library that is needed for serial communication
 
 #include "SparkFun_BNO080_Arduino_Library.h" //Including the library so we can access all the functions in the BNO080 library
